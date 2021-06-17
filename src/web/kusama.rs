1a3ebe0bdb3429a4d3de67ca7285d142ee1f3594
yktseng <tanistseng@gmail.com>
2021-06-17 19:58:58 +0800

    add a formal api to get validators

--- src/web/kusama.rs
+++ src/web/kusama.rs
@@ -10,12 +10,32 @@ struct ValidDetailOptions {
     option: String,
 }
 
+#[derive(Deserialize)]
+struct AllValidatorOptions {
+    size: Option<u32>,
+    page: Option<u32>
+}
+
 fn get_validators() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let path = warp::path("api")
         .and(warp::path("validators"))
         .and(warp::path::end())
         .map(|| warp::reply::json(&cache::get_validators("KSM")));
     path
+}
+
+fn get_all_validators_formal(db: Database) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("api")
+    .and(warp::path("v1"))
+    .and(warp::path("validators"))
+    .and(warp::path("KSM"))
+    .and(warp::path::end())
+    .and(with_db(db.clone()))
+    .and(warp::query::<AllValidatorOptions>())
+    .and_then(|db: Database, p: AllValidatorOptions| async move {
+        let chain_info = db.get_chain_info().await.unwrap();
+        get_data_from_db(db, chain_info.active_era, p.size, p.page).await
+    })
 }
 
 fn get_validator_trend(
@@ -72,8 +92,10 @@ fn get_validator_unclaimed_eras(
 async fn get_data_from_db(
     db: Database,
     era: u32,
+    size: Option<u32>,
+    page: Option<u32>,
 ) -> Result<warp::reply::WithStatus<warp::reply::Json>, Infallible> {
-    let result = db.get_all_validator_info_of_era(era, 0, 2000).await;
+    let result = db.get_all_validator_info_of_era(era, page.unwrap_or(0), size.unwrap_or(1500)).await;
     Ok(warp::reply::with_status(
         warp::reply::json(&result.unwrap()),
         StatusCode::OK,
@@ -186,19 +208,24 @@ pub fn routes(
         .or(get_1kv_nominators())
         .or(get_validator_unclaimed_eras(db.clone()))
         .or(get_stash_rewards(db.clone()))
-        .or(warp::path("api")
-            .and(warp::path("allValidators"))
-            .and(warp::path::end())
-            .and(with_db(db.clone()))
-            .and(warp::query::<HashMap<String, String>>())
-            .and_then(|db: Database, p: HashMap<String, String>| async move {
-                match p.get("size") {
-                    Some(_) => {
-                        let chain_info = db.get_chain_info().await.unwrap();
-                        get_data_from_db(db, chain_info.active_era).await
-                    }
-                    None => handle_query_parameter_err().await,
-                }
-            }));
+        .or(get_all_validators_formal(db.clone()))
+        .or(get_all_validators(db.clone()));
     routes
+}
+
+fn get_all_validators(db: Database) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path("api")
+    .and(warp::path("allValidators"))
+    .and(warp::path::end())
+    .and(with_db(db.clone()))
+    .and(warp::query::<HashMap<String, String>>())
+    .and_then(|db: Database, p: HashMap<String, String>| async move {
+        match p.get("size") {
+            Some(_) => {
+                let chain_info = db.get_chain_info().await.unwrap();
+                get_data_from_db(db, chain_info.active_era, None, None).await
+            }
+            None => handle_query_parameter_err().await,
+        }
+    })
 }
